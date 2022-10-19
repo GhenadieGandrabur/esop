e627bdd24086a82ea2ef82ac597ee8eac8517253
Ghenadie Gandrabur <ghenadiigandrabur@gmail.com>
2022-10-19 08:56:22 +0300

    some changes

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -3,6 +3,7 @@
   
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
@@ -27,7 +28,7 @@
     
     
     
-    <div class="container-fluid mb-2" >
+    <div class="header" >
       <div class="row">
         <div class="col-2 " >
       <img src="/img/olga.jpg" width="100%" height="100%">
@@ -73,12 +74,7 @@
         </div>
         
       </div>
-    </div>
-    
-
-    
-    
-    
+    </div>    
     
     <?php include 'footer.html.php'?>
     
